e2f41989051a92d553c6d1b9f3d7de8cf5b30129
MiraclE4502 <152858862+MiraclE4502@users.noreply.github.com>
2024-02-04 16:44:27 +0100

    Add files via upload
    Pages.github clone

---
+++ Github.pages/js/script.js
@@ -0,0 +1,10 @@
+const bestplayers = []
+
+bestplayers.push(prompt("Enter you best player name: "));
+bestplayers.push(prompt("Enter you best player name: "));
+bestplayers.push(prompt("Enter you best player name: "));
+bestplayers.push(prompt("Enter you best player name: "));
+bestplayers.push(prompt("Enter you best player name: "));
+
+
+console.log(bestplayers)
